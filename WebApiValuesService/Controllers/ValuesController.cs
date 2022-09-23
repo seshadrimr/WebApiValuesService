7a5a0806e71e63301d15c9f0b75b53180d32d85a
seshadrimr <seshadri.mr@gmail.com>
2022-09-23 13:55:20 +0530

    Update ValuesController.cs

--- WebApiValuesService/Controllers/ValuesController.cs
+++ WebApiValuesService/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApiValuesService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4" };
+            return new string[] { "value1", "value2", "value3", "value41" };
         }
 
         // GET api/values/5
