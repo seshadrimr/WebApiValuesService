d319b6352fd2780ff729bddcd88f3ead7fa571d4
seshadrimr <seshadri.mr@gmail.com>
2023-06-02 15:32:19 +0530

    Update ValuesController.cs

--- WebApiValuesService/Controllers/ValuesController.cs
+++ WebApiValuesService/Controllers/ValuesController.cs
@@ -32,7 +32,7 @@ namespace WebApiValuesService.Controllers
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public ActionResult<string> Put(int id, [FromBody] string value)
+        public ActionResult<string> Put(int id)
         {
             return id.ToString();
         }
